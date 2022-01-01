20ba7225747b180c1240216a9b5f5f29af687c10
mdmaruf641 <mdmarufmoni50@gmail.com>
2022-01-01 20:00:36 +0600

    branch test

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-    return (
-        <div>
-            <h1> Home page </h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>This is Home page </h1>
+    </div>
+  );
 };
 
 export default Home;
