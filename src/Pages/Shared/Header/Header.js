0c6966d410949879512d47e58ec2c741b499a312
unknown <ashleyalam33@gmail.com>
2022-01-01 20:20:03 +0600

    trial

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-            <h1> Header section</h1>
+            <h1> Header section push</h1>
         </div>
     );
 };
