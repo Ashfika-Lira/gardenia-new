34e126a7bb56de031614275e409d8a1f0d0180d4
mdmaruf641 <mdmarufmoni50@gmail.com>
2022-01-01 20:44:59 +0600

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import './App.css';
-import Home from './Pages/Home/Home/Home';
+import React from "react";
+import "./App.css";
+import Home from "./Pages/Home/Home/Home";
+import Header from "./Pages/Shared/Header/Header";
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <Header />
     </div>
   );
 }
