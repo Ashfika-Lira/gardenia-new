0a44e913687d979ee8b34903f81e3490c9ba709f
Tofazzal322 <tofazzal.tk322@gmail.com>
2022-01-01 23:00:18 +0400

    App

--- .history/src/App_20220101224132.js
+++ .history/src/App_20220101224132.js
@@ -8,11 +8,13 @@ import Header from "./Pages/Shared/Header/Header";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      
+          <BrowserRouter>
+        
         <Routes>
-          <Route path="/" element={<Home></Home>}></Route>
-          <Route path="/home" element={<Home></Home>}></Route>
-          <Route path="/header" element={<Header></Header>}></Route>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/home' element={<Home></Home>}></Route>
+          <Route path='/header' element={<Header></Header>}></Route>
           {/* <Route path='/Contact' element={<Contact></Contact>}></Route> */}
           {/* <Route path='/Shop' element={<Shop></Shop>}></Route> */}
           {/* <Route path='/fqa' element={<FQA></FQA>}></Route> */}
