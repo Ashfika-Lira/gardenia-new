62284b1e81e591e570705a66545027784cf0aefd
Tofazzal322 <tofazzal.tk322@gmail.com>
2022-01-02 00:01:20 +0400

    Bug fixing

--- .history/src/Pages/Shared/Navigation/Navigation_20220102000044.js
+++ .history/src/Pages/Shared/Navigation/Navigation_20220102000044.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Nav, Navbar } from 'react-bootstrap';
+import { Nav, Navbar } from 'react-bootstrap';
 import { HashLink } from 'react-router-hash-link';
 
 const Navigation = () => {
