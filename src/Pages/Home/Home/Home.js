e71c2a89b12ea717d3a0877cd1d13555ec7ad1e9
ohidurgclan <ohidur.ru.2019@gmail.com>
2022-01-02 02:23:17 +0600

    redux data setup

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { useGetAllPostQuery } from "../../../hooks/useProducts";
 
 const Home = () => {
-
+    const { data } = useGetAllPostQuery();
+    console.log(data)
     return (
-        <div>
-            <h1> Tofazzal  Home page </h1>
-        </div>
+        <>
+            {
+                data && data.map((items) => <h2>{items.name}</h2>)
+            }
+        </>
     );
 };
 
