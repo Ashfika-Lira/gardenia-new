48088a3e83f83bd341397a4243315e89412acd37
mdmaruf641 <mdmarufmoni50@gmail.com>
2022-01-01 20:24:16 +0600

    for marge test

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
-    return (
-        <div>
-            <h1> Header section</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1> Header section</h1>
+      <h4>ব্রাঞ্চ এত পেরা দেয় কেন?</h4>
+    </div>
+  );
 };
 
 export default Header;
