8aa94c4607d9363d404e64af269c99f022535e5f
Tofazzal322 <tofazzal.tk322@gmail.com>
2022-01-02 22:18:03 +0400

    Login And Register Page Created

---
+++ .history/src/Pages/Shared/Navigation/Navigation_20220102203116.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import {Button, Nav, Navbar } from 'react-bootstrap';
+import { HashLink } from 'react-router-hash-link';
+import useAuth from '../../../hooks/useAuth';
+
+const Navigation = () => {
+  const {user}= useAuth()
+   
+  return (
+    <Navbar
+      className="px-4 navigation-bar-main"
+      collapseOnSelect
+      expand="lg"
+      variant="light"
+    >
+      <Navbar.Brand as={HashLink} to="/Home">
+        Gardenia
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
+      <Navbar.Collapse id="responsive-navbar-nav">
+      
+        <Nav>
+          <Nav.Link as={HashLink} to="/shops">
+            Shops
+          </Nav.Link>
+          <Nav.Link as={HashLink} to="/offers">
+            Offers
+          </Nav.Link>
+          <Nav.Link as={HashLink} to="/FQA">
+            FQA
+          </Nav.Link>
+          <Nav.Link as={HashLink} to="/Contact">
+            Contact
+          </Nav.Link>
+          {
+!user?  <Nav.Link as={HashLink} to="/Login">
+            Login
+          </Nav.Link>:
+          }
+         
+          <Nav.Link >
+            Logout
+            
+          </Nav.Link>
+        </Nav>
+        
+      </Navbar.Collapse>
+  
+    </Navbar>
+       
+
+    );
+};
+ 
+export default Navigation;
+
