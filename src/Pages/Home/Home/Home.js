1bdeb7fd4575cb032ab2835c3830a205027dbc18
unknown <ashleyalam33@gmail.com>
2022-01-02 22:22:12 +0600

    header added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { useGetAllPostQuery } from "../../../hooks/useProducts";
+import Header from "../../Shared/Header/Header";
 
 const Home = () => {
     const { data } = useGetAllPostQuery();
     console.log(data)
     return (
         <>
+            <Header></Header>
             {
                 data && data.map((items) => <h2>{items.name}</h2>)
             }
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
 import React from 'react';
-import "./Header.css";
+// import "./Header.css";
 import { NavLink } from 'react-router-dom';
-import logo from '../../../images/download.png';
+import logo from '../../../images/download (2).png';
+import logo1 from '../../../images/istockphoto-1138644570-612x612.jpg';
 
 const Header = () => {
 	return (
@@ -18,12 +18,18 @@ const Header = () => {
     </div>
 			<div className="col-md-6">
             <div className="menu-container ">
-			<input className="form-control" type="text" placeholder="Search" aria-label="Search" />
+			<div className="d-flex mt-4">
+            <input className="form-control rounded-pill" type="text" placeholder="Search here" aria-label="Search" />
+            <button className='bg-warning rounded-pill w-25'>Search Now</button>
+            </div>
 		   </div>
 		</div>
 			<div className="col-md-3">
             <div className="menu-container ">
-			<h5>Shopping Cart</h5>
+            <div className="d-flex mt-1">
+            <img className="w-25" src={logo1} alt="" />
+			<h5 className="mt-3">Shopping Cart</h5>
+            </div>
 		   </div>
 		</div>
 
@@ -31,20 +37,5 @@ const Header = () => {
     </div>
   </div>
 	);
-=======
-import React from "react";
-
-const Header = () => {
-
-
-    return (
-        <div>
-            <h1> Header section push</h1>
-            
-        </div>
-    );
-
->>>>>>> 95031f58b2fcc74f12fd7b0f37eca2fa26bea054
 };
-
 export default Header;
