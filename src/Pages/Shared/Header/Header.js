d2d6f88a282591a266fdae25de4611db747875a2
ohidurgclan <ohidur.ru.2019@gmail.com>
2022-01-04 04:40:40 +0600

    header-fix

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Nav } from 'react-bootstrap';
 import { HashLink } from "react-router-hash-link";
-import logo1 from "../../../images/gardenia-logo.png";
+import logo from "../../../images/gardenia.png";
+import cart from "../../../images/gardenia-logo.png";
+import './Header.css';
 
 const Header = () => {
   return (
@@ -9,29 +11,31 @@ const Header = () => {
       <div className="MenuBar-container">
         <div className="container">
           <div className="row">
+            <div className="col-md-3">
+              <div className="gardenia-logo">
+                <img className="img img-fluid header-logo" src={logo} alt="" />
+              </div>
+            </div>
             <div className="col-md-6">
               <div className="menu-container ">
-                <div className="d-flex mt-1">
+                <div className="d-flex justify-content-end mt-1">
                   <input
-                    className="form-control rounded-pill"
+                    className="form-control rounded-pill position-relative"
                     type="text"
                     placeholder="Search here"
                     aria-label="Search"
                   />
-                  <button className="bg-warning rounded-pill w-25">
+                  <button className="srch-btn rounded-pill position-absolute">
                     Search Now
                   </button>
                 </div>
               </div>
             </div>
-            <div className="col-md-6">
-              <div className="menu-container ">
-                <div className="d-flex mt-1">
-                  <Nav.Link as={HashLink} to="/mycart">
-                    <img className="w-25" src={logo1} alt="" />
-                  </Nav.Link>
-                  <h5 className="mt-3">Shopping Cart</h5>
-                </div>
+            <div className="col-md-3">
+              <div className="mt-1">
+                <Nav.Link className="d-flex mt-1 justify-content-end" as={HashLink} to="/mycart">
+                  <img className="w-25" src={cart} alt="" />
+                </Nav.Link>
               </div>
             </div>
           </div>
