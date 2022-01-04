871065d76b1bda9534f87499002423838ef3961a
Tofazzal322 <tofazzal.tk322@gmail.com>
2022-01-04 19:41:08 +0400

    Bug Fixed

---
+++ .history/src/Pages/Login/PrivateRoute_20220104194043.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Spinner } from "react-bootstrap";
+import { useLocation } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+
+const PrivateRoute = ({ children, ...rest }) => {
+  const { user, isLoading } = useAuth();
+  let location = useLocation();
+  if (isLoading) {
+    return (.
+      <Spinner
+        sx={{
+          fontSize: "200px",
+          color: "red",
+          marginLeft: "500px",
+          marginTop: "200px",
+        }}
+      />
+    );
+  }
+  if (user.email) {
+    return children;
+  }
+  return <Navigate to="/home" state={{ from: location }} />;
+};
+
+export default PrivateRoute;
