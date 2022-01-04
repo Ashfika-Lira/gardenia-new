ce1b747c90848adbe3862577fa2d3a1399944b97
mdmaruf641 <mdmarufmoni50@gmail.com>
2022-01-04 21:59:14 +0600

    navigation some modified

--- src/Pages/Shared/Navigation/Navigation.js
+++ src/Pages/Shared/Navigation/Navigation.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { Button, Nav, Navbar } from "react-bootstrap";
 import { HashLink } from "react-router-hash-link";
 import useAuth from "../../../hooks/useAuth";
@@ -15,54 +15,54 @@ const Navigation = () => {
       variant="light"
     >
       <Navbar.Brand as={HashLink} to="/Home">
-        <span className='brand-name'>Gardenia</span>
+        <span className="brand-name">Gardenia</span>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
 
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav>
-          <Nav.Link as={HashLink} to="/home">
+          <Nav.Link className="fs-5 text-dark " as={HashLink} to="/home">
             Home
           </Nav.Link>
-          <Nav.Link as={HashLink} to="/shop">
+          <Nav.Link className="fs-5 text-dark " as={HashLink} to="/shop">
             Shops
           </Nav.Link>
-          <Nav.Link as={HashLink} to="/shop">
+          <Nav.Link className="fs-5 text-dark " as={HashLink} to="/shop">
             Offers
           </Nav.Link>
-          <Nav.Link as={HashLink} to="/FQA">
+          <Nav.Link className="fs-5 text-dark " as={HashLink} to="/FQA">
             FQA
           </Nav.Link>
           {/* <Nav.Link as={HashLink} to="/Contact">
             Contact
           </Nav.Link> */}
-          {
-            user.email && <Nav.Link as={HashLink} to="/dashboard">
-            Dashboard
-          </Nav.Link>
-          }
+          {user.email && (
+            <Nav.Link className="fs-5 text-dark " as={HashLink} to="/dashboard">
+              Dashboard
+            </Nav.Link>
+          )}
         </Nav>
-        <Nav className='ms-auto'>
-          
-          <Nav.Link as={HashLink} to="/Contact">
+        <Nav className="ms-auto">
+          <Nav.Link className="fs-5 text-dark " as={HashLink} to="/Contact">
             {user.displayName}
           </Nav.Link>
-          {
-            user.email? <Button
-                className=" nav-items-btn m-0 me-auto p-0 text-secondary "
-                onClick={logOut}
-                variant="button"
-              >
-                Logout
-              </Button>: <Nav.Link as={HashLink} to="/Login">
+          {user.email ? (
+            <Button
+              className=" nav-items-btn m-0 fs-5 text-dark me-auto p-0 "
+              onClick={logOut}
+              variant="button"
+            >
+              Logout
+            </Button>
+          ) : (
+            <Nav.Link className="fs-5 text-dark " as={HashLink} to="/Login">
               Login
             </Nav.Link>
-         }
+          )}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
   );
 };
- 
-export default Navigation;
 
+export default Navigation;
